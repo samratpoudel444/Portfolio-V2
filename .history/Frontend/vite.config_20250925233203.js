aec173b807f3717d7704f1cfe7d6ca9f6ba33078
samratpoudel444 <poudelsamrat444@gmail.com>
2025-09-25 23:41:16 +0545

    test

--- .history/Frontend/vite.config_20250925233203.js
+++ .history/Frontend/vite.config_20250925233203.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
-  
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build",
+  },
+});
