12607c72fbc39ef17665026bfb4364678f926b3b
samratpoudel444 <poudelsamrat444@gmail.com>
2025-11-08 20:06:23 +0545

    fixed config

--- .history/Frontend/vite.config_20251108200607.js
+++ .history/Frontend/vite.config_20251108200607.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "build", // <-- change from 'dist' to 'build'
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 });
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+});
